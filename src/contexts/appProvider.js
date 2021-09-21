b7c50e1f6602f5830ed7c1919aa4db3fcd5642b9
Breno Lopes <brenokos@gmail.com>
2021-09-21 15:52:41 -0300

    configurando o context

---
+++ src/contexts/appProvider.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import appContext from './appContext';
+
+const AppProvider = ({ children }) => {
+  const [state, setState] = useState('');
+
+  return (
+    <appContext.Provider value={ { state, setState } }>
+      {children}
+    </appContext.Provider>
+  );
+};
+
+AppProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default AppProvider;
