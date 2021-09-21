8d6ea905619d0ddf641a2e2b11aeddc519b87322
Julia Baptista <baptista.julia@yahoo.com.br>
2021-09-21 18:05:30 -0300

    Tea de login - requisitos 6 ate 8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Foods from './pages/Foods';
 import './App.css';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/comidas" component={ Foods } />
     </Switch>
   );
 }
---
+++ src/pages/Foods.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Foods = () => (
+  <p>Comidas</p>
+);
+
+export default Foods;
