25f65efdf60ceca49d5b6cf29c9f390bfdbc08bf
ALINE <hoshinoaline@yahoo.com.br>
2021-09-21 18:23:12 -0300

    requisito 3,4,5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => (
-  <div>
-    <input type="email" data-testid="email-input" />
-    <input type="password" data-testid="password-input" />
-    <button data-testid="login-submit-btn" type="button">Entrar</button>
-  </div>
-);
+const Login = () => {
+  const [email, setStateEmail] = useState('');
+  const [password, setStatePassword] = useState('');
+  // const [isDisabled, setIsDisabled] = useState(true)
+
+  function handleChange({ target: { value } }) {
+    setStateEmail(value);
+  }
+
+  function handleChange2({ target: { value } }) {
+    setStatePassword(value);
+  }
+
+  const validateEmail = () => {
+    const re = /\S+@\S+\.\S+/;
+    const passwordMin = 7;
+    const passwordCorrect = password.length >= passwordMin;
+    return re.test(email) && passwordCorrect;
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        name="email"
+        data-testid="email-input"
+        onChange={ handleChange }
+        value={ email }
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        name="password"
+        onChange={ handleChange2 }
+        value={ password }
+      />
+      <button
+        disabled={ !(validateEmail()) }
+        data-testid="login-submit-btn"
+        type="submit"
+      >
+        Entrar
+      </button>
+    </div>
+  );
+};
 
 export default Login;
