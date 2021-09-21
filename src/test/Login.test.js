2a14dca7ce3df1ba2b7ce791858a042bc7c6a19b
Juliane Marques <mqs.juliane@gmail.com>
2021-09-21 18:33:38 -0300

    Adiciona requisitos 2 ao 8

---
+++ src/test/Login.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from '../App';
+import Login from '../pages/Login';
+import { render } from '@testing-library/react';
+import renderWithRouter from '.RenderWithRouter';
+
+
+describe('Testando tela Login')
+// se a rota ta certa
+// testa titulo, inputs e botoes
+// se botao habilita com email senha correto
+//componentes na tela
+// botao comeca disabled
+// fireevent.type / click - se o input escreveuo que digitei (olhar no state)
