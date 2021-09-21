f0e3d7e458d412a782037a12884c64bd5b076877
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-09-21 18:27:35 -0300

    update req. 9

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Provider from './context/provider';
-import Header from './components/Header';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
           {/* <Route exact path="/" component={ Login } /> */}
         </Switch>
       </BrowserRouter>
-      <Header />
     </Provider>
   );
 }
