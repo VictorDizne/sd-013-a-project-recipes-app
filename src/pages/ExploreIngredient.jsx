dcabafff320dbcdc1c0514c41d8467e123d3761e
Demistocles <69822263+Demistocles@users.noreply.github.com>
2021-09-21 20:52:22 -0300

    [GRUPO - 21] - requisito 19 ao 24 (#76)
    * requisito 19 ao 24 - Footer
    
    * correçao lint

--- src/pages/ExploreIngredient.jsx
+++ src/pages/ExploreIngredient.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 function ExploreIngredient() {
   return (
     <div>
       Explore Ingredient
+      <Footer />
     </div>
 
   );
