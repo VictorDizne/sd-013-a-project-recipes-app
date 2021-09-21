c76b39b57a10fc4c2d9682ae167695bad2578b92
JeanVPacheco <jeanvitorp@gmail.com>
2021-09-21 18:26:26 -0300

    Adiciona ate requisito 12

---
+++ src/components/header.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import searchIcon from '../images/searchIcon.svg';
+import profileIcon from '../images/profileIcon.svg';
+import SearchBar from './searchbar';
+
+function Header({ title }) {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  function showSearchButton() {
+    // Primeiro checa se a página tem o título de Comidas ou Explorar Origem,
+    // a barra de procura deve ser mostrada somente nessa ocasião.
+    if (title === 'Comidas' || title === 'Explorar Origem') {
+      console.log('entrou');
+      // Caso esteja na página com título de Comidas, aparece um botão para ser
+      // clicado. Ao clicar nesse botão, é mudado o valor responsável por condicionar
+      // a renderização da barra de pesquisa
+      return (
+        <button type="button" onClick={ () => setShowSearchBar(!showSearchBar) }>
+          <img
+            src={ searchIcon }
+            alt="searchIcon"
+            data-testid="search-top-btn"
+          />
+        </button>);
+    } return null;
+  }
+
+  function searchBar() {
+    // Checa se deve mostrar a searchBar, se for negativo retorna nulo, se for
+    // positivo retorna o componente SearchBar
+    const searchBarComponent = showSearchBar ? <SearchBar /> : null;
+    return searchBarComponent;
+  }
+
+  return (
+    <header>
+      <Link to="/perfil">
+        <img
+          src={ profileIcon }
+          alt="profileIcon"
+          data-testid="profile-top-btn"
+        />
+      </Link>
+      <h1 data-testeid="page-title">{ title }</h1>
+      {}
+      { showSearchButton() }
+      { searchBar() }
+    </header>
+  );
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
