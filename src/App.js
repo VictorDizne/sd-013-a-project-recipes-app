2e11ea1cd999d7d9221c378b061b127e82a3d29b
Ricardo Sousa <rwmsousa@gmail.com>
2021-09-21 16:40:14 -0300

    Revert "comecando o projeto"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
+import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Switch } from 'react-router-dom';
-import { Login, Foods, Drinks } from './Pages/Index';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/comidas" component={ Foods } />
-      <Route exact path="/bebidas" component={ Drinks } />
-    </Switch>
+    <div className="meals">
+      <span className="logo">TRYBE</span>
+      <object
+        className="rocksGlass"
+        type="image/svg+xml"
+        data={ rockGlass }
+      >
+        Glass
+      </object>
+    </div>
   );
 }
 
--- src/Pages/Index.js
+++
@@ -1,5 +0,0 @@
-import Login from './Login';
-import Drinks from './Drinks';
-import Foods from './Foods';
-
-export { Login, Drinks, Foods };
