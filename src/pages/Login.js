5babe953ee8088cc0c09dfc389a5e26f74e69d3c
ArthurMGoulart <48330533+ArthurMGoulart@users.noreply.github.com>
2021-09-22 15:03:03 -0300

    Retirar importação não utilizada
    Para rodar o lint, retiramos a linha da importação do Header

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-import Header from '../components/header';
 
 const Login = (props) => {
   const [email, setEmail] = useState('');
