12d8d9b910d3076b24281c27a07ab89e3b29810e
LuizFJP <luizfjportela@gmail.com>
2021-09-22 16:40:40 -0300

    requirement 19 and 20

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,7 +1,92 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import appContext from '../contexts/appContext';
+import { fetchByIngredient, fetchByName, fetchByLetter } from '../services/fetchs';
 
 export default function SearchBar() {
+  const { state, setState } = useContext(appContext);
+
+  const [searchText, setSearchText] = useState('');
+  const [option, setOption] = useState('ingredient');
+
+  const onSearch = async () => {
+    const { history } = props;
+    if (history.location === '/comidas') {
+      let foods;
+      switch (option) {
+      case 'ingredient':
+        foods = await fetchByIngredient(searchText);
+        setState({ ...state, foods: [...foods] });
+        break;
+      case 'name':
+        foods = await fetchByName(searchText);
+        setState({ ...state, foods: [...foods] });
+        break;
+      case 'first-letter':
+        if (searchText.length > 1) {
+          global.alert('Sua busca deve conter somente 1 (um) caracter');
+        } else {
+          foods = await fetchByLetter(searchText);
+          setState({ ...state, foods: [...foods] });
+        }
+        break;
+      default:
+        break;
+      }
+    } else if ()
+  };
+
   return (
-    <input type="text" data-testid="search-input" />
+    <form>
+      <label htmlFor="search-text">
+        <input
+          id="search-text"
+          type="text"
+          data-testid="search-input"
+          placeHolder="Buscar Receita"
+          onChange={ (e) => setSearchText(e.target.value) }
+        />
+      </label>
+      <label htmlFor="search-ingredient">
+        Ingrediente
+        <input
+          id="search-ingredient"
+          type="radio"
+          data-testid="ingredient-search-radio"
+          name="search-type"
+          value="ingredient"
+          onClick={ (e) => setOption(e.target.value) }
+        />
+      </label>
+      <label htmlFor="search-name">
+        Nome
+        <input
+          id="search-name"
+          type="radio"
+          data-testid="name-search-radio"
+          name="search-type"
+          value="name"
+          onClick={ (e) => setOption(e.target.value) }
+        />
+      </label>
+      <label htmlFor="search-first-letter">
+        Primeira Letra
+        <input
+          id="search-first-letter"
+          type="radio"
+          data-testid="first-letter-search-radio"
+          name="search-type"
+          value="first-letter"
+          onClick={ (e) => setOption(e.target.value) }
+        />
+      </label>
+      <button
+        id="button-login"
+        type="button"
+        data-testid="exec-search-btn"
+        onClick={ onSearch }
+      >
+        Buscar
+      </button>
+    </form>
   );
 }
--- src/pages/HomeFood.js
+++ src/pages/HomeFood.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/header';
+import LowerMenu from '../components/LowerMenu';
 
 const HomeFood = () => (
   <div>
     <Header name="Comidas" search />
     Home Food
+    <LowerMenu />
   </div>
 );
 
