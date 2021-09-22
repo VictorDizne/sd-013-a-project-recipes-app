4c2ef78557637db0f5ff52622a91d5cffe5ae51c
PC <sthecso>
2021-09-22 19:30:30 -0300

    Requisito 10-em andamento

---
+++ src/pages/ExploreMealsArea.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function ExploreMealsArea() {
+  return (
+    <Header />
+  );
+}
+
+export default ExploreMealsArea;
---
+++ src/pages/IngredientsDrinksExplore.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function IgredientsDrinkExplore() {
+  return (
+    <Header />
+  );
+}
+
+export default IgredientsDrinkExplore;
