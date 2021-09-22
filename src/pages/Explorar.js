d83136129f236ff392c1f119e9042ca439da3a1d
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-09-22 19:35:53 -0300

    Adiciona requisitos 67 e 68

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
 
 const Explorar = () => (
   <div>
     <HeaderWithoutSearch page="Explorar" />
+    <div data-testid="explore-food">
+      <Link to="/explorar/comidas">
+        <button type="submit">Explorar Comidas</button>
+      </Link>
+    </div>
+    <div data-testid="explore-drinks">
+      <Link to="/explorar/bebidas">
+        <button type="submit">Explorar Bebidas</button>
+      </Link>
+    </div>
     <Footer />
   </div>
 );
