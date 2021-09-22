38c48a27b7f5e997ef65f59e618e23ea5ea9762f
Diego Vilela <vilelaphp@gmail.com>
2021-09-22 15:27:47 -0300

    Cria todas as rotas

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -8,6 +8,31 @@ function Routes() {
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/comidas" component={ Comidas } />
+      <Route path="/comidas/:recipeId" component={ Comidas } />
+      <Route path="/bebidas/:recipeId" component={ Comidas } />
+      <Route
+        path="/comidas/:recipeId/in-progress"
+        component={ Comidas }
+      />
+      <Route
+        path="/bebidas/:recipeId/in-progress"
+        component={ Comidas }
+      />
+      <Route path="/explorar" component={ Comidas } />
+      <Route path="/explorar/comidas" component={ Comidas } />
+      <Route path="/explorar/bebidas" component={ Comidas } />
+      <Route
+        path="/explorar/comidas/ingredientes"
+        component={ Comidas }
+      />
+      <Route
+        path="/explorar/bebidas/ingredientes"
+        component={ Comidas }
+      />
+      <Route path="/explorar/comidas/area" component={ Comidas } />
+      <Route path="/perfil" component={ Comidas } />
+      <Route path="/receitas-feitas" component={ Comidas } />
+      <Route path="/receitas-faviritas" component={ Comidas } />
     </Switch>
   );
 }
