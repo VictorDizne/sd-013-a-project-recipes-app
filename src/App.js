059d37b4221be62ab07ee55d8c02fc7e902342f6
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-09-22 13:57:31 -0300

    update in routes

--- src/App.js
+++ src/App.js
@@ -2,46 +2,57 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Provider from './context/provider';
-import {
-  Login,
-  TelaComidas,
-  TelaBebidas,
-  DetalheComidas,
-  DetalheBebidas,
-  ReceitasProcessoComidas,
-  ReceitasProcessoBebidas,
-  Explorar,
-  ExplorarComidas,
-  ExplorarBebidas,
-  IngredienteComidas,
-  IngredienteBebidas,
-  ExplorarComidasArea,
-  Perfil,
-  ReceitasFeitas,
-  ReceitasFavoritas,
-} from './pages';
+import pages from './pages/indexPages';
 
 function App() {
   return (
     <Provider>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/comidas" component={ TelaComidas } />
-          <Route exact path="/bebidas" component={ TelaBebidas } />
-          <Route exact path="/comidas/{id-da-receita}" component={ DetalheComidas } />
-          <Route exact path="/bebidas/{id-da-receita}" component={ DetalheBebidas } />
-          <Route exact path="comidas/{id-da-receita}/in-progress" component={ ReceitasProcessoComidas } />
-          <Route exact path="bebidas/{id-da-receita}/in-progress" component={ ReceitasProcessoBebidas } />
-          <Route exact path="/explorar" component={ Explorar } />
-          <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
-          <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
-          <Route exact path="/explorar/comidas/ingredientes" component={ IngredienteComidas } />
-          <Route exact path="/explorar/bebidas/ingredientes" component={ IngredienteBebidas } />
-          <Route exact path="/explorar/comidas/area" component={ ExplorarComidasArea } />
-          <Route exact path="/perfil" component={ Perfil } />
-          <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
-          <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
+          <Route exact path="/" component={ pages.Login } />
+          <Route exact path="/perfil" component={ pages.Perfil } />
+          <Route exact path="/explorar" component={ pages.Explorar } />
+          <Route exact path="/comidas" component={ pages.TelaComidas } />
+          <Route exact path="/bebidas" component={ pages.TelaBebidas } />
+          <Route exact path="/receitas-feitas" component={ pages.ReceitasFeitas } />
+          <Route exact path="/explorar/comidas" component={ pages.ExplorarComidas } />
+          <Route exact path="/explorar/bebidas" component={ pages.ExplorarBebidas } />
+          <Route exact path="/receitas-favoritas" component={ pages.ReceitasFavoritas } />
+          <Route
+            exact
+            path="/comidas/{id-da-receita}"
+            component={ pages.DetalheComidas }
+          />
+          <Route
+            exact
+            path="/bebidas/{id-da-receita}"
+            component={ pages.DetalheBebidas }
+          />
+          <Route
+            exact
+            path="/explorar/comidas/area"
+            component={ pages.ExplorarComidasArea }
+          />
+          <Route
+            exact
+            path="comidas/{id-da-receita}/in-progress"
+            component={ pages.ReceitasProcessoComidas }
+          />
+          <Route
+            exact
+            path="bebidas/{id-da-receita}/in-progress"
+            component={ pages.ReceitasProcessoBebidas }
+          />
+          <Route
+            exact
+            path="/explorar/comidas/ingredientes"
+            component={ pages.IngredienteComidas }
+          />
+          <Route
+            exact
+            path="/explorar/bebidas/ingredientes"
+            component={ pages.IngredienteBebidas }
+          />
         </Switch>
       </BrowserRouter>
     </Provider>
