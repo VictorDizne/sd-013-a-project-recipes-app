5ae35b4ab462038c6e4d7c5299c7c2b6d97eab86
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-09-23 17:31:38 -0300

    G22-In-Progress-Page

--- src/pages/Food/FoodInProgress.jsx
+++ src/pages/Food/FoodInProgress.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { InProgressFinishButton, InProgressHeader, InProgressImage,
+  InProgressIngredients, InProgressInstructions } from '../../components/InProgress';
+import { fetchMealById } from '../../services/API';
+import '../../CSS/InProgress.css';
 
 function FoodInProgress() {
+  const dispatch = useDispatch();
+  const recipe = useSelector((state) => state.api.recipe);
+  const { id } = useParams();
+
+  useEffect(() => { // Faz o fetch caso a pessoa acesse diretamente pela URL (sem passar pela tela de detalhes)
+    if (!recipe) {
+      fetchMealById(id, dispatch);
+    }
+  }, [dispatch, id, recipe]);
+
+  if (!recipe) return null;
   return (
-    <div>
-      Food In Progress Page
-    </div>
+    <>
+      <InProgressImage spec="Meal" />
+      <InProgressHeader spec="Meal" />
+      <InProgressIngredients spec="Meal" />
+      <InProgressInstructions />
+      <InProgressFinishButton spec="Meal" />
+    </>
   );
 }
 
