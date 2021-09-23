c3286056c4ed7eec9f0518f2c82429162aa3e6fb
mcamillosf <matheuscamillo@hotmail.com>
2021-09-23 20:00:12 -0300

    criando css da tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
 import { setUserEmail } from '../redux/actions/index';
+import '../styles/login.css';
+import loginIcon from '../images/loginIcon.svg';
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -40,30 +42,35 @@ const Login = () => {
   }, [state]);
 
   return (
-    <form action="">
-      <input
-        type="text"
-        data-testid="email-input"
-        name="email"
-        value={ state.email }
-        onChange={ handleChange }
-      />
-      <input
-        type="password"
-        data-testid="password-input"
-        name="password"
-        value={ state.password }
-        onChange={ handleChange }
-      />
-      <button
-        type="submit"
-        data-testid="login-submit-btn"
-        disabled={ !valid }
-        onClick={ handleSubmit }
-      >
-        Entrar
-      </button>
-    </form>
+    <div className="form-login">
+      <img src={ loginIcon } alt="login icon" />
+      <form action="" className="form">
+        <input
+          type="text"
+          data-testid="email-input"
+          name="email"
+          value={ state.email }
+          onChange={ handleChange }
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          value={ state.password }
+          onChange={ handleChange }
+          placeholder="Password"
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-btn"
+          disabled={ !valid }
+          onClick={ handleSubmit }
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
   );
 };
 
