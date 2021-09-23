a3a056199805670beeab6a87ca29b80710522174
janderson555 <jandinsm@gmail.com>
2021-09-23 16:46:29 -0300

    Corrigi footer, adiciona header parcialmente

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../App.css';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import metalIcon from '../images/mealIcon.svg';
+
+import '../PaginasCss/Footer.css';
 
 const Footer = () => (
   <div data-testid="footer" className="Footer">
     <Link
-      to="/bebidas"
+      to="/comidas"
       type="button"
-      data-testid="drinks-bottom-btn"
+      data-testid="food-bottom-btn"
+      src={ metalIcon }
     >
-      <img src="src/images/drinkIcon.svg" alt="icone de bebida" />
+      <img src={ metalIcon } alt="icone de comida" />
     </Link>
     <Link
       to="/explorar"
       type="button"
       data-testid="explore-bottom-btn"
+      src={ exploreIcon }
     >
-      <img src="src/images/exploreIcon.svg" alt="icone de explorar" />
+      <img src={ exploreIcon } alt="icone de explorar" />
     </Link>
     <Link
-      to="/comidas"
+      to="/bebidas"
       type="button"
-      data-testid="food-bottom-btn"
+      data-testid="drinks-bottom-btn"
+      src={ drinkIcon }
     >
-      <img src="src/images/mealIcon.svg" alt="icone de comida" />
+      <img src={ drinkIcon } alt="icone de bebida" />
     </Link>
   </div>
 );
