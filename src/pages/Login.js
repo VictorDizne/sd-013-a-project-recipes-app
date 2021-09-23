2d6cb7dadddfc98b29db922a2c828366d43a28c9
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-09-23 14:03:15 -0300

    alter

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,32 +1,39 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-
 function Login() {
-  // const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
-
 
   const userLogin = () => {
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
     localStorage.setItem('user', JSON.stringify({ email }));
     history.push('/comidas');
-
   };
 
   function userValid() {
-    const userPass = 6;
-    const checkEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/
-      .test(email);
-
-    const chekcPass = password.length > userPass;
-    const verify = checkEmail && chekcPass ? true : false;
-    return verify;
+    const passLength = 7;
+    const validPassword = password.length >= passLength;
+    const validEmail = /^[a-z0-9_]+@[a-z]+\.[a-z]{2,3}(?:\.[a-z]{2})?$/i.test(
+      email,
+    );
+    return validEmail && validPassword;
   }
+  // const userValid = () => {
+  //   const userPass = 6;
+  //   if (password.length <= userPass) return false;
+  //   if (/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(email)) return false;
+  //   return true;
+  // };
 
+  // const checkEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/
+  //   .test(email);
+
+  // const chekcPass = password.length > userPass;
+  // const verify = checkEmail && chekcPass ? trureturn : false;
+  // return verify;
   return (
     <section>
       <form>
