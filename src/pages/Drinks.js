d1dd301da2ca62e2ca4a87ceea6ce3679c960195
Julia Baptista <baptista.julia@yahoo.com.br>
2021-09-23 13:02:43 -0300

    Footer

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import RecipesContext from '../context/index';
 import Header from '../components/Header';
 import RecipeCard from '../components/RecipeCard';
+import Footer from '../components/Footer';
 
 const Drinks = () => {
   const { data } = useContext(RecipesContext);
@@ -10,9 +11,10 @@ const Drinks = () => {
   return (
     <div>
       <Header title="Bebidas" hasSearchIcon page="drinks" />
-      { data.slice(0, MAX_RECIPES).map(((recipe, index) => (
+      { data && data.slice(0, MAX_RECIPES).map(((recipe, index) => (
         <RecipeCard key={ index } index={ index } recipe={ recipe } page="drinks" />
       )))}
+      <Footer />
     </div>
   );
 };
--- src/pages/FoodByLocal.js
+++ src/pages/FoodByLocal.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const FoodByLocal = () => (
-  <Header title="Explorar Origem" hasSearchIcon />
+  <div>
+    <Header title="Explorar Origem" hasSearchIcon />
+    <Footer />
+  </div>
 );
 
 export default FoodByLocal;
