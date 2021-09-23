ba8afb0ac77c93f9b1552d904bf5cd374e9404ec
Felipe Dias <felipeferreiradias1@gmail.com>
2021-09-23 19:18:13 -0300

    fix lint

--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
 
 function ExplorarComidas() {
-  const { meal, setMeal } = useState([]);
+  /*  const { meal, setMeal } = useState([]); */
   /* const { idMeal } = sortMeal; */
 
   const fetchAPI = async () => {
     const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
     const { meals } = await fetch(url).then((response) => response.json());
     console.log(meals);
-   /*  setMeal(meals); */
+    /*  setMeal(meals); */
   };
 
   useEffect(() => {
@@ -41,7 +41,7 @@ function ExplorarComidas() {
         </Link>
       </div>
       <div>
-        <Link to={ 'comidas/' }>
+        <Link to="/comidas">
           <button
             type="submit"
             data-testid="explore-surprise"
