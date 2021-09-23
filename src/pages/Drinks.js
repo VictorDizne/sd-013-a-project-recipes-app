c4127c9d2609b9d5f639353e0212460d030bcf39
Tatiane Chiarello <tatichiarello@gmail.com>
2021-09-23 14:46:51 +0200

    altera forma de receber apenas 12 receitas

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,31 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import RecipesContext from '../context/index';
 import Header from '../components/Header';
 import RecipeCard from '../components/RecipeCard';
 
 const Drinks = () => {
   const { data } = useContext(RecipesContext);
-  const [newData, setNewData] = useState([]);
-
-  useEffect(() => {
-    const newArray = [];
-    const MAX_RECIPES = 11;
-    for (let i = 0; i <= MAX_RECIPES; i += 1) {
-      if (data[i]) {
-        newArray.push(data[i]);
-      }
-    }
-    setNewData(newArray);
-  }, [data]);
-
+  console.log(data);
+  const MAX_RECIPES = 12;
   return (
     <div>
       <Header title="Bebidas" hasSearchIcon page="drinks" />
-      {
-        newData.map(((recipe, index) => (
-          <RecipeCard key={ index } index={ index } recipe={ recipe } />
-        )))
-      }
+      { data.slice(0, MAX_RECIPES).map(((recipe, index) => (
+        <RecipeCard key={ index } index={ index } recipe={ recipe } page="drinks" />
+      )))}
     </div>
   );
 };
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,31 +1,17 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import RecipesContext from '../context/index';
 import Header from '../components/Header';
 import RecipeCard from '../components/RecipeCard';
 
 const Foods = () => {
   const { data } = useContext(RecipesContext);
-  const [newData, setNewData] = useState([]);
-
-  useEffect(() => {
-    const newArray = [];
-    const MAX_RECIPES = 11;
-    for (let i = 0; i <= MAX_RECIPES; i += 1) {
-      if (data[i]) {
-        newArray.push(data[i]);
-      }
-    }
-    setNewData(newArray);
-  }, [data]);
-
+  const MAX_RECIPES = 12;
   return (
     <div>
       <Header title="Comidas" hasSearchIcon page="foods" />
-      {
-        newData.map(((recipe, index) => (
-          <RecipeCard key={ index } index={ index } recipe={ recipe } page="foods" />
-        )))
-      }
+      { data !== null && data.slice(0, MAX_RECIPES).map(((recipe, index) => (
+        <RecipeCard key={ index } index={ index } recipe={ recipe } page="foods" />
+      )))}
     </div>
   );
 };
