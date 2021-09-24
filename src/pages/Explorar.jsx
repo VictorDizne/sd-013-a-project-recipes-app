feffc412361d432173d454399189854770b05dd1
Matheus Macedo <matheus.castro.macedo.xi@gmail.com>
2021-09-24 15:32:33 -0300

    Finaliza tela de explorar

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
-const Explorar = () => <Footer />;
+const Explorar = () => (
+  <div>
+    <Link to="/explorar/comidas">
+      <button data-testid="explore-food" type="button"> Explorar Comidas</button>
+    </Link>
+    <Link to="/explorar/bebidas">
+      <button data-testid="explore-drinks" type="button"> Explorar Bebidas </button>
+    </Link>
+    <Footer />
+  </div>
+);
+
 export default Explorar;
