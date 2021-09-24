b54ce1fabb59d4545f692c2642f0e293795af84a
ArthurMGoulart <budiarthur@gmail.com>
2021-09-24 17:11:07 -0300

    Fechando a branch

--- src/components/ReadyMealsCards.jsx
+++ src/components/ReadyMealsCards.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
 
 function ReadyReceipesCards() {
   return (
     <section>
-      <img data-testid={`${index}-horizontal-image`} sr="" alt="" />
-      <p data-testid={`${index}-horizontal-top-text`}>
+      <Link to={ `/comidas${id}` }>
+        <img data-testid={ `${index}-horizontal-image` } src="" alt="" />
+      </Link>
+      <p data-testid={ `${index}-horizontal-top-text` }>
         texto da categoria da receita
       </p>
-      <p data-testid={`${index}-horizontal-name'}>
-       texto do nome da receita
-      </p>
-      <p data-testid={`${index}-horizontal-done-date`}>
+      <Link to={ `/comidas${id}` }>
+        <p data-testid={ `${index}-horizontal-name` }>
+          texto do nome da receita
+        </p>
+      </Link>
+      <p data-testid={ `${index}-horizontal-done-date` }>
         texto da data que a receita foi feita
       </p>
-      <button type="button" data-testid={`${index}-horizontal-share-btn`}>
-        botão de compartilhar
+      <button type="button" data-testid={ `${index}-horizontal-share-btn` }>
+        <img src={ shareIcon } alt="drink-icon" />
       </button>
-      <span data-testid={`${index}-${tagName}-horizonta-tag`}>
+      <span data-testid={ `${index}-${tagName}-horizontal-tag` }>
         TAGS
       </span>
-    </
+    </section>
+  );
 }
 
 export default ReadyReceipesCards;
