ac9d5ee88df486ab3d787f220a0f5b0ae3e65154
Matheus Macedo <matheus.castro.macedo.xi@gmail.com>
2021-09-24 16:10:05 -0300

    3/4 da tela de explorar-comidas-bebidas

--- src/pages/ExplorarBebidas.jsx
+++ src/pages/ExplorarBebidas.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
-const ExplorarBebida = () => <Footer />;
-
+const ExplorarBebida = () => (
+  <div>
+    <Link to="/explorar/bebidas/ingredientes">
+      <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    </Link>
+    <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
+    <Footer />
+  </div>
+);
 export default ExplorarBebida;
