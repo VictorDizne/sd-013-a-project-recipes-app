f73869d9af76f62e8688098adbf3aee63f85f160
Diego Vilela <vilelaphp@gmail.com>
2021-09-24 17:23:20 -0300

    Implementa o requisito 33

--- src/pages/BebidasDetalhe.js
+++ src/pages/BebidasDetalhe.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import fetchDrinkById from '../services/fetchDrinkById';
 
 function BebidasDetalhes({ match }) {
   const { recipeId } = match.params;
+
+  const [drink, setDrink] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getDrink = async (id) => {
+      const result = await fetchDrinkById(id);
+      setDrink(result);
+    };
+    getDrink(recipeId);
+    setIsLoading(false);
+  }, [setDrink, recipeId]);
+
+  if (isLoading) return <h1>Loading...</h1>;
+
   return (
     <div>
       <h1>
-        BebidasDetalhes
+        Detalhes da Receita de Drink
         {recipeId}
       </h1>
+      <h2 data-testid="recipe-title">{drink.strDrink}</h2>
+      <h3 data-testid="recipe-category">{drink.strCategory}</h3>
+      <img
+        src={ drink.strDrinkThumb }
+        alt={ drink.strDrink }
+        data-testid="recipe-photo"
+      />
+      <button type="button" data-testid="share-btn">Share</button>
+      <button type="button" data-testid="favorite-btn">S2</button>
+
+      <h4>Ingredientes</h4>
+      <ul>
+        <li data-testid="0-ingredient-name-and-measure">
+          <p>Iterar sobre as chaves `strIngredient1` até `strIngredient20` aqui.</p>
+          <p>Concatenar com `strMeasure1` ao `strMeasure20`</p>
+        </li>
+      </ul>
+
+      <h4>Instruções</h4>
+      <p data-testid="instructions">{drink.strInstructions}</p>
+
+      <h4>Recomendações</h4>
+      <p data-testid="0-recomendation-card">Ver o que é isso.</p>
+
+      <button type="button" data-testid="start-recipe-btn">
+        Iniciar
+      </button>
     </div>
   );
 }
--- src/pages/ComidasDetalhe.js
+++ src/pages/ComidasDetalhe.js
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import fetchMealById from '../services/fetchMealById';
 
 function ComidasDetalhes({ match }) {
   const { recipeId } = match.params;
+
+  const [meal, setMeal] = useState({});
+
+  useEffect(() => {
+    const getMeal = async (id) => {
+      const result = await fetchMealById(id);
+      setMeal(result);
+    };
+    getMeal(recipeId);
+  }, [setMeal, recipeId]);
+
   return (
     <div>
       <h1>
-        ComidasDetalhes
+        Detalhes da Receita de Comida
         {recipeId}
       </h1>
+      <h2 data-testid="recipe-title">{meal.strMeal}</h2>
+      <h3 data-testid="recipe-category">{meal.strCategory}</h3>
+      <img
+        src={ meal.strMealThumb }
+        alt={ meal.strMeal }
+        data-testid="recipe-photo"
+      />
+      <button type="button" data-testid="share-btn">Share</button>
+      <button type="button" data-testid="favorite-btn">S2</button>
+
+      <h4>Ingredientes</h4>
+      <ul>
+        <li data-testid="0-ingredient-name-and-measure">
+          <p>Iterar sobre as chaves `strIngredient1` até `strIngredient20` aqui.</p>
+          <p>Concatenar com `strMeasure1` ao `strMeasure20`</p>
+        </li>
+      </ul>
+
+      <h4>Instruções</h4>
+      <p data-testid="instructions">{meal.strInstructions}</p>
+
+      <iframe
+        width="560"
+        height="315"
+        src={ meal.strYoutube }
+        title={ meal.strMeal }
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        data-testid="video"
+      />
+
+      <h4>Recomendações</h4>
+      <p data-testid="0-recomendation-card">Ver o que é isso.</p>
+
+      <button type="button" data-testid="start-recipe-btn">
+        Iniciar
+      </button>
     </div>
   );
 }
