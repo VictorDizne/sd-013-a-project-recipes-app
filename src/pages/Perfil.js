5a16d18bfcf08a0c40ece0a546ba9d60afc65702
Tais Carvalho <taispcarvalho@exemplo.br>
2021-09-24 12:14:19 -0300

    Profile Page

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Perfil() {
+  const history = useHistory();
+
+  const storageUserEmail = localStorage.getItem('user');
+  const userEmail = JSON.parse(storageUserEmail);
+  console.log(userEmail.email);
+
+  const logout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <>
       <Header />
+      <main>
+        <span data-testid="profile-email">{`Email: ${userEmail.email}`}</span>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ logout }
+        >
+          Sair
+        </button>
+      </main>
       <Footer />
     </>
   );
