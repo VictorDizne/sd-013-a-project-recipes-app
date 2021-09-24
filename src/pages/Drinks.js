d13fe31b75b0529bef394e30294dbe5ba9eb1e4e
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-09-23 21:21:32 -0300

    Requisito 16 a 18

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,31 +1,58 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router';
 import Context from '../Context/Context';
 import useFetchApi from '../Hooks/useFetchAPI';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Drinks() {
-  const drink = 'thecocktail';
+  const urlDrink = 'thecocktail';
   const { data, recipes } = useContext(Context);
+  const history = useHistory();
   const secondButton = true;
-  useFetchApi(data.search, data.text, drink);
-  console.log(recipes.drinks);
+  useFetchApi(data.search, data.text, urlDrink);
+  const renderDrinks = () => {
+    const magic = 12;
+    if (recipes.drinks === null) {
+      return (
+        global.alert('Sinto muito, não encontramos nenhuma receita para esses filtros.')
+      );
+    }
+    if (recipes.drinks.length === 1) {
+      const id = recipes.drinks[0].idDrink;
+      return history.push(`./bebidas/${id}`);
+    }
+    if (recipes.drinks.length > magic) {
+      const cooktail = recipes.drinks.slice(0, magic);
+      return cooktail.map((recipe, index) => (
+        <div key={ index } data-testid={ `${index}-recipe-card` }>
+          <h3 data-testid={ `${index}-card-name` }>{ recipe.strDrink }</h3>
+          <img
+            src={ recipe.strDrinkThumb }
+            alt={ recipe.strDrink }
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      ));
+    }
+    return (
+      recipes.drinks.map((drink, index) => (
+        <div key={ index } data-testid={ `${index}-recipe-card` }>
+          <h3 data-testid={ `${index}-card-name` }>{ drink.strMeal }</h3>
+          <img
+            src={ drink.strMealThumb }
+            alt={ drink.strMeal }
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      ))
+    );
+  };
   return (
     <section>
       <Header text="Bebidas" secondButton={ secondButton } />
       <h1>Drinks</h1>
-      { recipes.drinks === undefined ? null
-        : (
-          recipes.drinks.map((recipe, index) => (
-            <div key={ index } data-testid={ `${index}-recipe-card` }>
-              <h3 data-testid={ `${index}-card-name` }>{ recipe.strDrink }</h3>
-              <img
-                src={ recipe.strDrinkThumb }
-                alt={ recipe.strDrink }
-                data-testid={ `${index}-card-img` }
-              />
-            </div>
-          )))}
+      { recipes.drinks !== undefined ? renderDrinks() : null }
       <Footer />
     </section>
   );
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,43 +1,57 @@
-import React, { useContext, useHistory } from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router';
 import Context from '../Context/Context';
 import Header from '../components/Header';
 import useFetchApi from '../Hooks/useFetchAPI';
 import Footer from '../components/Footer';
-/* import PropTypes from 'prop-types'; */
-
-const renderFoods = () => {
-  const { recipes: { meals }, setIdFood } = useContext(Context);
-  const history = useHistory();
-
-  if (meals.length === 1) {
-    setIdFood(meals[0].idMeal);
-
-  };
-
-
-}
 
 function Foods() {
-  const food = 'themeal';
-  const { data } = useContext(Context);
+  const { recipes, data } = useContext(Context);
+  const history = useHistory();
+  const urlFood = 'themeal';
   const secondButton = true;
-  useFetchApi(data.search, data.text, food);
-  console.log(recipes.meals);
+  useFetchApi(data.search, data.text, urlFood);
+  const renderFoods = () => {
+    const magic = 12;
+    if (recipes.meals === null) {
+      return (
+        global.alert('Sinto muito, não encontramos nenhuma receita para esses filtros.')
+      );
+    }
+    if (recipes.meals.length === 1) {
+      const id = recipes.meals[0].idMeal;
+      return history.push(`./comidas/${id}`);
+    }
+    if (recipes.meals.length > magic) {
+      const foods = recipes.meals.slice(0, magic);
+      return foods.map((food, index) => (
+        <div key={ index } data-testid={ `${index}-recipe-card` }>
+          <h3 data-testid={ `${index}-card-name` }>{ food.strMeal }</h3>
+          <img
+            src={ food.strMealThumb }
+            alt={ food.strMeal }
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      ));
+    }
+    return (
+      recipes.meals.map((recipe, index) => (
+        <div key={ index } data-testid={ `${index}-recipe-card` }>
+          <h3 data-testid={ `${index}-card-name` }>{ recipe.strMeal }</h3>
+          <img
+            src={ recipe.strMealThumb }
+            alt={ recipe.strMeal }
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      ))
+    );
+  };
   return (
     <div>
       <Header text="Comidas" secondButton={ secondButton } />
-      { recipes.meals === undefined ? null
-        : (
-          recipes.meals.map((recipe, index) => (
-            <div key={ index } data-testid={ `${index}-recipe-card` }>
-              <h3 data-testid={ `${index}-card-name` }>{ recipe.strMeal }</h3>
-              <img
-                src={ recipe.strMealThumb }
-                alt={ recipe.strMeal }
-                data-testid={ `${index}-card-img` }
-              />
-            </div>
-          )))}
+      { recipes.meals !== undefined ? renderFoods() : null }
       <Footer />
     </div>
   );
