48ee0142fdcb8cd7fe4d433d9c2635cfa99823e0
Wesley Maia <maia.weol@gmail.com>
2021-09-24 11:30:35 -0300

    Requirement 17 with APIs (Meals and Drinks) - in process

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -3,11 +3,12 @@ import React from 'react';
 
 export default function ReciperCard({ name, index, img }) {
   return (
-    <div data-testid={ `${index}-recipe-card` }>
+    <div data-testid={ `${index}-recipe-card` } className="card">
       <img
         data-testid={ `${index}-card-img` }
         src={ img }
         alt={ name }
+        className="imgThumb"
       />
       <p data-testid={ `${index}-card-name` }>{ name }</p>
     </div>
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-// import ReciperCard from '../components/RecipeCard';
+import { MainContext } from '../context/Provider';
+import ReciperCard from '../components/RecipeCard';
 
 function Meals() {
+  const { recipesMeals } = useContext(MainContext);
+  const maxList = 12;
+  let recipesList = recipesMeals;
+
+  if (recipesMeals.length > maxList) {
+    recipesList = recipesMeals.slice(1, maxList + 1);
+  }
+
   return (
     <>
       <Header />
+      {recipesList.map((recipe, index) => (
+        <ReciperCard
+          key={ recipe.idMeal }
+          index={ index }
+          name={ recipe.strMeal }
+          img={ recipe.strMealThumb }
+        />))}
       <Footer />
     </>
   );
