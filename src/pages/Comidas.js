a2c192d7f235cd06eeff21117bcd4957625f439e
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-09-24 17:41:14 -0300

    Inicio tela de receita em progresso

--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,34 +1,14 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Context from '../context';
-import fetchMeals from '../services/fetchMeals';
-import fetchMealsCategories from '../services/fetchMealsCategories';
 import MealsList from '../components/MealsList';
 import MealsCategories from '../components/MealsCategories';
 
 function Comidas() {
-  const { setMeals, setMealsCategories, setFilteredMeals } = useContext(Context);
+  const { filteredMeals } = useContext(Context);
 
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const getMeals = async () => {
-      const results = await fetchMeals();
-      setMeals(results);
-      setFilteredMeals(results);
-    };
-    getMeals();
-
-    const getMealsCategories = async () => {
-      const results = await fetchMealsCategories();
-      setMealsCategories(results);
-    };
-    getMealsCategories();
-    setIsLoading(false);
-  }, [setMeals, setMealsCategories, setFilteredMeals]);
-
-  if (isLoading) return <h1>Loading...</h1>;
+  if (filteredMeals.length === 0) return <h1>Loading...</h1>;
 
   return (
     <>
