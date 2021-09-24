a37f5cfa3d17a587a344e436e796902c038f15e6
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-09-24 19:09:11 -0300

    Completa requisitos da pagina de perfil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
+// import appContext from '../redux/appcontext';
 
-const Perfil = () => (
-  <div>
-    <HeaderWithoutSearch page="Perfil" />
-    <Footer />
-  </div>
-);
+const Perfil = () => {
+  const [email, setEmail] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const userEmail = JSON.parse(localStorage.getItem('user'));
+    setEmail(userEmail.email);
+  }, []);
+
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
+
+  return (
+    <div>
+      <HeaderWithoutSearch page="Perfil" />
+      <h4 data-testid="profile-email">{email}</h4>
+      <div>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ handleLogout }
+        >
+          Sair
+        </button>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Perfil;
