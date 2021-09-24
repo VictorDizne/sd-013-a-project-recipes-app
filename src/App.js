33171ed61dabaed0e71b8deb5bf2d503b09af67b
Lucas Lima <lucassauro.lt@gmail.com>
2021-09-24 00:38:18 -0300

    Add Requirement 16, 17 and 18. Not tested.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchBar from './Components/SearchBar';
-import MealDetails from './Pages/FoodDetails';
-import CocktailDetails from './Pages/DrinkDetails';
+import MealDetails from './Pages/Foods';
+import CocktailDetails from './Pages/Drinks';
+import ContextAPIProvider from './ContextAPI/ContextAPI';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" />
-        <Route exact path="/bebidas" component={ SearchBar } />
-        <Route exact path="/bebidas/:id" component={ CocktailDetails } />
-        <Route exact path="/comidas" component={ SearchBar } />
-        <Route exact path="/comidas/:id" component={ MealDetails } />
-      </Switch>
-    </BrowserRouter>
+    <ContextAPIProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" />
+          <Route exact path="/bebidas" component={ SearchBar } />
+          <Route exact path="/bebidas/:id" component={ CocktailDetails } />
+          <Route exact path="/comidas" component={ SearchBar } />
+          <Route exact path="/comidas/:id" component={ MealDetails } />
+        </Switch>
+      </BrowserRouter>
+    </ContextAPIProvider>
   );
 }
 
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,17 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import Context from '../ContextAPI/Context';
+import RecipeCard from './RecipeCard';
 
-import {
-  fetchMealIngredient,
-  fetchMealName,
-  fetchMealFirstLetter,
-} from '../services/mealAPI';
-
-import {
-  fetchCocktailIngredient,
-  fetchCocktailName,
-  fetchCocktailFirstLetter,
-} from '../services/cocktailAPI';
+// import {
+//   fetchIngredient,
+//   fetchName,
+//   fetchFirstLetter,
+// } from '../services/fetchAPI';
 
 const SearchBar = () => {
   const [input, setInput] = useState({
@@ -22,93 +18,104 @@ const SearchBar = () => {
     setInput({ ...input, [id]: value });
   };
 
-  const location = useLocation().pathname;
+  const { fetchIngredient,
+    fetchName,
+    fetchFirstLetter,
+    data,
+  } = useContext(Context);
+
+  const { pathname } = useLocation();
+
+  const pathnameCheck = () => {
+    switch (pathname) {
+    case '/comidas':
+      return 'themealdb';
+    case '/bebidas':
+      return 'thecocktaildb';
+    default:
+      return null;
+    }
+  };
 
   const verifyRadioValue = (value) => {
     const { inputText } = input;
-
-    switch (location) {
-    case '/comidas':
-      if (value === 'ingredient') {
-        fetchMealIngredient(inputText);
-      } else if (value === 'name') {
-        fetchMealName(inputText);
-      } else if (value === 'first-letter') {
-        if (inputText.length >= 2) {
-          global.alert('Sua busca deve conter somente 1 (um) caracter');
-        }
-        fetchMealFirstLetter(inputText);
+    if (value === 'ingredient') {
+      fetchIngredient(pathnameCheck(), inputText);
+    } else if (value === 'name') {
+      fetchName(pathnameCheck(), inputText);
+    } else if (value === 'first-letter') {
+      if (inputText.length >= 2) {
+        global.alert('Sua busca deve conter somente 1 (um) caracter');
       }
-      break;
-
-    case '/bebidas':
-      if (value === 'ingredient') {
-        fetchCocktailIngredient(inputText);
-      } else if (value === 'name') {
-        fetchCocktailName(inputText);
-      } else if (value === 'first-letter') {
-        if (inputText.length >= 2) {
-          global.alert('Sua busca deve conter somente 1 (um) caracter');
-        }
-        fetchCocktailFirstLetter(inputText);
-      }
-      break;
-
-    default: return null;
+      fetchFirstLetter(pathnameCheck(), inputText);
     }
   };
-  const onChangeValue = (event) => (verifyRadioValue(event.target.value));
+
+  const onMouseClick = (event) => (verifyRadioValue(event.target.value));
+
+  const DOZE = 12;
 
   const { inputText } = input;
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Buscar"
-        data-testid="search-input"
-        id="inputText"
-        value={ inputText }
-        onChange={ inputChange }
-      />
 
       <div>
-        <label htmlFor="ingredient-search">
-          <input
-            type="radio"
-            id="ingredient-search"
-            name="button"
-            value="ingredient"
-            data-testid="ingredient-search-radio"
-            onClick={ onChangeValue }
-          />
-          Ingrediente
-        </label>
+        <input
+          type="text"
+          placeholder="Buscar"
+          data-testid="search-input"
+          id="inputText"
+          value={ inputText }
+          onChange={ inputChange }
+        />
 
-        <label htmlFor="name-search">
-          <input
-            type="radio"
-            id="name-search"
-            name="button"
-            value="name"
-            data-testid="name-search-radio"
-            onClick={ onChangeValue }
-          />
-          Nome
-        </label>
+        <div>
+          <label htmlFor="ingredient-search">
+            <input
+              type="radio"
+              id="ingredient-search"
+              name="button"
+              value="ingredient"
+              data-testid="ingredient-search-radio"
+              onClick={ onMouseClick }
+            />
+            Ingrediente
+          </label>
 
-        <label htmlFor="first-letter-search">
-          <input
-            type="radio"
-            id="first-letter-search"
-            name="button"
-            value="first-letter"
-            data-testid="first-letter-search-radio"
-            onClick={ onChangeValue }
-          />
-          Primeira letra
-        </label>
+          <label htmlFor="name-search">
+            <input
+              type="radio"
+              id="name-search"
+              name="button"
+              value="name"
+              data-testid="name-search-radio"
+              onClick={ onMouseClick }
+            />
+            Nome
+          </label>
+
+          <label htmlFor="first-letter-search">
+            <input
+              type="radio"
+              id="first-letter-search"
+              name="button"
+              value="first-letter"
+              data-testid="first-letter-search-radio"
+              onClick={ onMouseClick }
+            />
+            Primeira letra
+          </label>
+        </div>
       </div>
+      {data.map((recipe, index) => (
+        index < DOZE && <RecipeCard
+          name={ recipe.strDrink || recipe.strMeal }
+          img={ recipe.strDrinkThumb || recipe.strMealThumb }
+          key={ index }
+        />
+      ))}
     </div>
+
   );
 };
 
