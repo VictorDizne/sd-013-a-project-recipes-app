653f1b7e279ec79fe896371f91a6c6a8185c4353
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-09-24 21:17:39 -0400

    Requisito 74

--- src/pages/ExplorarBebidas.js
+++ src/pages/ExplorarBebidas.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDebugState } from 'use-named-state';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import ComponentFooter from '../components/ComponentFooter';
 
 function ExplorarBebidas() {
+  const [bebidaID, setBebidaID] = useDebugState('bebidaID', 0);
+
+  useEffect(() => {
+    async function fetchData() {
+      const URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+      const fetchApi = await fetch(URL);
+      const data = await fetchApi.json();
+      setBebidaID(data.drinks[0].idDrink);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Header title="Explorar Bebidas" hideSearch hideProfile={ false } />
@@ -15,12 +28,11 @@ function ExplorarBebidas() {
       </Link>
       <Link
         data-testid="explore-surprise"
-        to="/"
+        to={ `/bebidas/${bebidaID}` }
       >
         Me Surpreenda!
       </Link>
       <ComponentFooter />
-
     </div>
   );
 }
--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDebugState } from 'use-named-state';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import ComponentFooter from '../components/ComponentFooter';
 
 function ExplorarComidas() {
+  const [comidaID, setComidaID] = useDebugState('comidaID', 0);
+
+  useEffect(() => {
+    async function fetchData() {
+      const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+      const fetchApi = await fetch(URL);
+      const data = await fetchApi.json();
+      setComidaID(data.meals[0].idMeal);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Header title="Explorar Comidas " hideSearch hideProfile={ false } />
@@ -19,7 +32,12 @@ function ExplorarComidas() {
       >
         Por Local de Origem
       </Link>
-      <Link data-testid="explore-surprise" to="/">Me Surpreenda!</Link>
+      <Link
+        data-testid="explore-surprise"
+        to={ `/comidas/${comidaID}` }
+      >
+        Me Surpreenda!
+      </Link>
       <ComponentFooter />
     </div>
   );
