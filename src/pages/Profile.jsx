e0285fe61956963c5d48849df7158ed5eddcb497
Matheus Duarte de Freitas <freickup@gmail.com>
2021-09-27 17:07:45 -0300

    finalizacao do Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,39 +1,43 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Header, Footer, Button } from '../components/index'
+import { Header, Footer, Button } from '../components/index';
 
-function Profile{
-
+function Profile() {
   const getUserEmail = () => {
     if (localStorage.getItm('user')) {
       return JSON.parse(localStorage.getItem('user'));
     }
-  }
-  
+  };
+
   const history = useHistory();
 
   return (
     <div>
       <Header title="Perfil" />
-      <h1></h1>
+      <h1
+        data-testid="profile-email"
+      >
+        { getUserEmail() }
+      </h1>
       <Button
         className="doneRecipes"
         type="button"
         buttonText="Receitas Feitas"
-        onClick={ history.push('/receitas-feitas') } 
+        onClick={ history.push('/receitas-feitas') }
       />
       <Button
         className="favoriteRecipes"
         type="button"
         buttonText="Receitas Favoritas"
-        onClick={ history.push('/receitas-favoritas') } 
-      />   
+        onClick={ history.push('/receitas-favoritas') }
+      />
       <Button
         className="logoutButton"
         type="button"
         buttonText="Sair"
-        onClick={ history.push('/') } 
+        onClick={ history.push('/') }
       />
+      <Footer />
     </div>
   );
 }
