937d45e467cea7c5a20fffb17195b6f3e8551e44
Matheus Duarte de Freitas <freickup@gmail.com>
2021-09-27 16:54:45 -0300

    Inicio da Page Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Header, Footer, Button } from '../components/index'
 
-const Profile = () => (
+function Profile{
+
   const getUserEmail = () => {
     if (localStorage.getItm('user')) {
       return JSON.parse(localStorage.getItem('user'));
     }
   }
-
+  
   const history = useHistory();
 
-);
+  return (
+    <div>
+      <Header title="Perfil" />
+      <Button
+        className="doneRecipes"
+        type="button"
+         
+      />
+      <h1></h1>
+      <Button />
+    </div>
+  );
+
+}
 
 export default Profile;
