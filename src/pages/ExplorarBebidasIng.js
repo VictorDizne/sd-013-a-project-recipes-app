87ad7906fba818eaddd082645f32a2ede78aecbd
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-09-27 16:46:46 -0300

    Adiciona requisitos  75 e 76

--- src/pages/ExplorarBebidasIng.js
+++ src/pages/ExplorarBebidasIng.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
 import Footer from '../components/Footer';
 
-const ExplorarBebidasIng = () => (
-  <div>
-    <HeaderWithoutSearch page="Explorar Ingredientes" />
-    <Footer />
-  </div>
-);
+const ExplorarBebidasIng = () => {
+  const [ingredientsDrinks, setIngredientsDrinks] = useState([]);
+
+  async function fecthDrinks() {
+    const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+    const result = await fetch(endpoint).then((res) => res.json());
+    const slicedResult = result.drinks.slice(0, Number('12'));
+    setIngredientsDrinks(slicedResult);
+  }
+
+  useEffect(() => {
+    fecthDrinks();
+  }, []);
+
+  return (
+    <div>
+      <HeaderWithoutSearch page="Explorar Bebidas" />
+      {ingredientsDrinks.map((drink, index) => (
+        <button
+          type="submit"
+          key={ index }
+          data-testid={ `${index}-ingredient-card` }
+        >
+          <h3 data-testid={ `${index}-card-name` }>{ drink.strIngredient1}</h3>
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ `https://www.thecocktaildb.com/images/ingredients/${drink.strIngredient1}-Small.png` }
+            alt={ drink.strIngredient1 }
+            /* width="150px" */
+            // `https://www.thecocktaildb.com/images/ingredients/${ingredient}-Small.png`
+          />
+        </button>
+      ))}
+      <Footer />
+    </div>
+  );
+};
 
 export default ExplorarBebidasIng;
--- src/pages/ExplorarComidasIng.js
+++ src/pages/ExplorarComidasIng.js
@@ -4,22 +4,36 @@ import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
 import Footer from '../components/Footer';
 
 const ExplorarComidasIng = () => {
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredientsMeals, setIngredientsMeals] = useState([]);
+
+  async function fecthIngredients() {
+    const endpoint = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+    const result = await fetch(endpoint).then((res) => res.json());
+    const slicedResult = result.meals.slice(0, Number('12'));
+    setIngredientsMeals(slicedResult);
+  }
 
   useEffect(() => {
-    async function fecthIngredients() {
-      const endpoint = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
-      const result = await fetch(endpoint).then((res) => res.json());
-      const slicedResult = result.meals.slice(0, Number('12'));
-      setIngredients(slicedResult);
-      console.log(ingredients);
-    }
     fecthIngredients();
   }, []);
 
   return (
     <div>
       <HeaderWithoutSearch page="Explorar Ingredientes" />
+      {ingredientsMeals.map((ingredient, index) => (
+        <button
+          type="submit"
+          key={ index }
+          data-testid={ `${index}-ingredient-card` }
+        >
+          <h3 data-testid={ `${index}-card-name` }>{ ingredient.strIngredient}</h3>
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ `https://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png` }
+            alt={ ingredient.strIngredient }
+          />
+        </button>
+      ))}
       <Footer />
     </div>
   );
