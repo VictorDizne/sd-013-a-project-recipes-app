4fb647092a99bc135cf1c60dadc5316d84ae334d
Gessé Carlos <gesse.dev@outlook.com>
2021-09-27 12:35:16 -0300

    componente header criado e lib react icons instalada

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function Header() {
+  return (
+    <header>
+      <h1>oi</h1>
+    </header>
+  );
+}
+
+export default Header;
