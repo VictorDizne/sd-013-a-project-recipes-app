c85232b1ab7c7c8254b964505b8e512887d38776
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-09-27 15:35:45 -0300

    alter

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import * as APIendpoints from '../services/APIendpoints';
 
 function Foods() {
+  const obj = [];
+  const [foods, setFoods] = useState(obj);
+
+  useEffect(() => {
+    const fetchFoods = async () => {
+      const sizeFoods = 12;
+      const results = await APIendpoints.searchFood('');
+      setFoods(results.filter((result, index) => index < sizeFoods));
+    };
+    fetchFoods();
+    if (foods === obj) return;
+    setFoods(obj);
+  }, [obj]);
   return (
     <div>
       <Header />
