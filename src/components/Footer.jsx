8f7a1c20590187310c2a9c0cc80184cac19d3429
andzoni <lorenzoniandre@outlook.com>
2021-09-27 22:45:25 +0100

    Finish footer-component-task

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { Button } from '.';
+import { drinkIcon, exploreIcon, mealIcon } from '../images';
 
 function Footer() {
+  const history = useHistory();
+
   return (
-    <div />
+    <footer className="footer" data-testid="footer">
+      <Button
+        id="food-bottom-btn"
+        onClick={ history.push('/comidas') }
+        buttonText={
+          <img
+            src={ mealIcon }
+            alt="Fork and spoon Icon"
+            className="foodsBtnIcon"
+          />
+        }
+      />
+    </footer>
   );
 }
 
