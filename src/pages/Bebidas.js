5e8e9d82cc4ec4977e16b5e1742e98397f5c99b5
Diego Vilela <vilelaphp@gmail.com>
2021-09-27 16:40:53 -0300

    Bug fix nas páginas comidas e bebidas

--- src/pages/Bebidas.js
+++ src/pages/Bebidas.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import DrinksList from '../components/DrinksList';
 import DrinksCategories from '../components/DrinksCategories';
+import Context from '../context';
 
 function Bebidas() {
+  const { filteredDrinks } = useContext(Context);
+
+  if (filteredDrinks.length === 0) return <h1>Loading...</h1>;
+
   return (
     <>
       <Header tela="Bebidas" />
