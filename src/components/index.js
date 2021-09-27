dbf5c37e5da523870e8b723f48cbc211d7b432db
Gessé Carlos <gesse.dev@outlook.com>
2021-09-27 19:07:39 -0300

    lint fixed in index js from components and profile from pages

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,2 @@
 export { default as Input } from './Input';
 export { default as Button } from './Button';
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Header, Footer, Button } from '../components/index';
+import { Button } from '../components/index';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Profile() {
   const getUserEmail = () => {
