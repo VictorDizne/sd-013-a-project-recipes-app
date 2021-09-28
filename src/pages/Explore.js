433a5b8df6021598d1ad7f3a31bca31b5f491cd2
Micael Santos <micaelmaicon@hotmail.com>
2021-09-28 16:09:49 -0300

    Req 67 to 69 - Finish

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Explore() {
+  const history = useHistory();
+  const hadleExploreDrink = () => history.push('/explorar/bebidas');
+  const handleExploreMeals = () => history.push('/explorar/comidas');
+
   return (
     <>
       <Header />
-      <button type="button" data-testid="explore-food">Explorar Comidas</button>
-      <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      <button
+        data-testid="explore-drinks"
+        type="button"
+        onClick={ hadleExploreDrink }
+      >
+        Explorar Bebidas
+      </button>
+      <button
+        onClick={ handleExploreMeals }
+        type="button"
+        data-testid="explore-food"
+      >
+        Explorar Comidas
+      </button>
       <Footer />
     </>
   );
