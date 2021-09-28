98e503faf71f35f9dd1474fa6bb5b5803e61c652
Cassyel <cassyel1967@gmail.com>
2021-09-28 16:44:03 -0300

    Details part 1

--- src/components/ComponentDetails.js
+++ src/components/ComponentDetails.js
@@ -1,9 +1,10 @@
+import PropTypes from 'prop-types';
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import recipeContext from '../context';
 import ComponentDetailsCard from './ComponentDetailsCard';
 
-function ComponentDetails() {
+function ComponentDetails({ drinkOrMeal }) {
   const { fetchDetails, details, dataForFetch: { currentPage },
     handleCurrentPage } = useContext(recipeContext).ContextDetails;
   const { id } = useParams();
@@ -14,30 +15,43 @@ function ComponentDetails() {
     if (currentPage !== '') fetchDetails(currentPage, 'lookup', 'i', id);
   }, [currentPage, id]);
 
-  const filterIngredients = () => {
-    const currentObject = Object.entries(details[0]);
-
-    const ingredientArray = currentObject.filter((ingredientKey) => ingredientKey[0]
-      .includes('strIngredient') && ingredientKey[1] !== '');
-
-    const renderIngredients = ingredientArray.map((item, index) => (
-      <p data-testid={ `${index}-ingredient-name-and-measure` } key={ item }>{item[1]}</p>
-    ));
-
-    return renderIngredients;
+  const arrayIntercale = (arr1, arr2, arr3 = []) => {
+    if (arr1.length === 0) return arr3;
+    arr3.push(arr1.shift());
+    if (arr2.length !== 0) arr3.push(arr2.shift());
+    return arrayIntercale(arr1, arr2, arr3);
   };
 
-  const filterMeasure = () => {
+  const filterIngredientsAndMeasures = () => {
     const currentObject = Object.entries(details[0]);
+    const ingredientArray = currentObject.filter((ingredientKey) => ingredientKey[0]
+      .includes('strIngredient') && ingredientKey[1] !== '' && ingredientKey[1] !== null);
+    const renderIngredients = ingredientArray.map((item) => item[1]);
 
-    const measureArray = currentObject.filter((measureKey) => measureKey[0]
-      .includes('strMeasure') && measureKey[1] !== '');
+    const currentObject2 = Object.entries(details[0]);
+    const measureArray = currentObject2.filter((measureKey) => measureKey[0]
+      .includes('strMeasure') && measureKey[1] !== ' ' && measureKey[1] !== null);
+    const renderMeasure = measureArray.map(((item) => item[1]));
 
-    const renderMeasure = measureArray.map((item) => (
-      <p key={ item }>{item[1]}</p>
-    ));
+    const arrayIntercaled = arrayIntercale(renderIngredients, renderMeasure);
+    return arrayIntercaled;
+  };
 
-    return renderMeasure;
+  const finalArray = () => {
+    const array = filterIngredientsAndMeasures();
+    const shortArrays = [];
+
+    while (array.length > 0) {
+      shortArrays.push(array.splice(0, 2));
+    }
+    const arrayMap = shortArrays.map((item, index) => (
+      <p
+        key={ index }
+        data-testid={ `${index}-ingredient-name-and-measure` }
+      >
+        {item.toString().replace(',', ' - ')}
+      </p>));
+    return arrayMap;
   };
 
   if (details === '') return <h1>Loading</h1>;
@@ -47,15 +61,19 @@ function ComponentDetails() {
         <ComponentDetailsCard
           key={ index }
           detailItem={ item }
+          renderIngredients={ finalArray }
+          drinkOrMeal={ drinkOrMeal }
         />
       )) }
-      {filterIngredients()}
-      {filterMeasure()}
       <button type="button" data-testid="share-btn">Compartilhar</button>
       <button type="button" data-testid="favorite-btn">Favoritar</button>
       <button data-testid="start-recipe-btn" type="button">Iniciar receita</button>
     </div>
   );
 }
+
+ComponentDetails.propTypes = {
+  drinkOrMeal: PropTypes.string.isRequired,
+};
 
 export default ComponentDetails;
--- src/components/ComponentDetailsCard.js
+++ src/components/ComponentDetailsCard.js
@@ -1,34 +1,86 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-function ComponentDetailsCard({ detailItem }) {
-  return (
+function ComponentDetailsCard({ detailItem, renderIngredients }) {
+  const history = useHistory();
+  const handlePage = history.location.pathname.includes('/comidas');
+
+  /* const renderVideo = () => {
+    if (history.location.pathname.includes('/comidas')) {
+      return (
+        <iframe
+          data-testid="video"
+          width="560"
+          height="315"
+          src={ video }
+          title={ detailItem.strMeal }
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write;
+        encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      );
+    }
+  }; */
+
+  const renderMeal = () => {
+    const video = detailItem.strYoutube.replace('watch?v=', 'embed/');
+    return (
+      <div>
+        <img
+          data-testid="recipe-photo"
+          src={ detailItem.strMealThumb }
+          alt={ detailItem.strMeal }
+        />
+        <h1 data-testid="recipe-title">{detailItem.strMeal}</h1>
+        <h4 data-testid="recipe-category">{detailItem.strCategory}</h4>
+        {renderIngredients()}
+        <p data-testid="instructions">{detailItem.strInstructions}</p>
+        <iframe
+          data-testid="video"
+          width="560"
+          height="315"
+          src={ video }
+          title={ detailItem.strMeal }
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write;
+        encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+    );
+  };
+
+  const renderDrink = () => (
     <div>
       <img
         data-testid="recipe-photo"
-        src={ detailItem.strMealThumb }
-        alt={ detailItem.strMeal }
+        src={ detailItem.strDrinkThumb }
+        alt={ detailItem.strDrink }
       />
-      <h1 data-testid="recipe-title">{detailItem.strMeal}</h1>
-      <h4 data-testid="recipe-category">{detailItem.strCategory}</h4>
+      <h1 data-testid="recipe-title">{detailItem.strDrink}</h1>
+      <h4 data-testid="recipe-category">
+        {detailItem.strCategory}
+        ,
+        {' '}
+        {detailItem.strAlcoholic}
+      </h4>
+      {renderIngredients()}
       <p data-testid="instructions">{detailItem.strInstructions}</p>
-      <iframe
-        data-testid="video"
-        width="560"
-        height="315"
-        src={ detailItem.strYoutube }
-        title={ detailItem.strMeal }
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write;
-        encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
+    </div>
+  );
+
+  return (
+    <div>
+      {handlePage ? renderMeal() : renderDrink()}
     </div>
   );
 }
 
 ComponentDetailsCard.propTypes = {
-  detailItem: PropTypes.objectOf(Object).isRequired,
+  detailItem: PropTypes.objectOf().isRequired,
+  renderIngredients: PropTypes.func.isRequired,
 };
 
 export default ComponentDetailsCard;
