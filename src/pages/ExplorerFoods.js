495e6d111ddc03f7b8b63aed566a2e14755c5224
Adriana Barreto <adri92barreto123@gmail.com>
2021-09-28 16:22:22 -0300

    Executa questao 73

--- src/pages/ExplorerFoods.js
+++ src/pages/ExplorerFoods.js
@@ -1,10 +1,23 @@
 import { useHistory } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExplorerFoods() {
   const history = useHistory();
+  // const [food, setfood] = useState();
+
+  // async function fetchAPI() {
+  //   const response = await fetch('www.themealdb.com/api/json/v1/1/random.php')
+  //     .then((data) => data.json())
+  //     .then((e) => e.meals.idMeal);
+  //   console.log(response);
+  //   return response;
+  // }
+
+  // useEffect(() => {
+  //   fetchAPI().then((resp) => setfood(resp));
+  // }, []);
 
   return (
     <div>
@@ -19,12 +32,15 @@ function ExplorerFoods() {
       <button
         type="button"
         data-testid="explore-by-area"
+        onClick={ () => history.push('/explorar/comidas/area') }
       >
         Por Local de Origem
       </button>
       <button
         type="button"
         data-testid="explore-surprise"
+        // onClick={ () => history.push(`/comidas/${food}`) }
+
       >
         Me Surpreenda!
       </button>
