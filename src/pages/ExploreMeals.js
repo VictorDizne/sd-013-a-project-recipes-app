0e3304d69c04ed623cd2acf8773964ceef63b6d3
Micael Santos <micaelmaicon@hotmail.com>
2021-09-28 17:35:03 -0300

    Req 70 to 71 - in progress

--- src/pages/ExploreMeals.js
+++ src/pages/ExploreMeals.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ExploreButtons from '../components/ExploreButtons';
 
 function ExploreMeals() {
   return (
     <>
       <Header />
+      <ExploreButtons />
       <Footer />
     </>
   );
