b0fec7478dbdcdc5d3dad1bb37535369d4857a16
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-09-29 17:08:26 -0300

    requisitos 70 a 74 funcionando

--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,12 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import ButtonsExplore from '../components/ButtonsExplore';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExplorarComidas({ history }) {
+  const isTrue = true;
+
+  const fetchRandomMeal = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then((response) => response.json())
+      .then(({ meals: [meal] }) => history.push(`/comidas/${meal.idMeal}`))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <>
       <Header pageTitle="Explorar Comidas" history={ history } />
+      <ButtonsExplore
+        isMeal={ isTrue }
+        recipeType="comidas"
+        fetchRandom={ fetchRandomMeal }
+      />
       <Footer />
     </>
   );
