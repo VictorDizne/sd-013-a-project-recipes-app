790f04a8e681fedc12dd05b18cc61a4f06568c1d
Rafhael Gomes <rafhael@protonmail.com>
2021-09-29 18:07:58 -0300

    req 54 55 56 57 58 59

---
+++ src/components/RecipeDoneCard.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Alert } from '@mui/material';
+import { Link } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+import './css/Card.css';
+
+const copy = require('clipboard-copy');
+
+const RecipeDoneCard = ({ recipe, index, history }) => {
+  const { type } = recipe;
+
+  const handleShareBtn = (recipeId, recipeType, recipeIndex) => {
+    const isMeal = recipeType === 'comida';
+    copy(`http://localhost:3000/${isMeal ? 'comidas/' : 'bebidas/'}${recipeId}`);
+
+    const h4 = document.createElement('h4');
+    h4.textContent = 'Link copiado!';
+    const father = document
+      .querySelector(`[data-testid="${recipeIndex}-horizontal-share-btn"]`);
+    father.insertAdjacentElement('afterend', h4);
+    // <Alert variant="filled" severity="success">Link copiado!</Alert>);
+  };
+
+  const handleTitleImgClick = (rec) => {
+    const isMeal = rec.type === 'comida';
+    history.push(`/${isMeal ? 'comidas/' : 'bebidas/'}${rec.id}`);
+  };
+
+  return (
+    <div>
+      <input
+        type="image"
+        alt="alguma coisa"
+        data-testid={ `${index}-horizontal-image` }
+        src={ recipe.image }
+        onClick={ () => handleTitleImgClick(recipe) }
+        className="recipe-image"
+      />
+      <p
+        data-testid={ `${index}-horizontal-top-text` }
+      >
+        { type === 'comida'
+          ? `${recipe.area} - ${recipe.category}` : recipe.alcoholicOrNot }
+      </p>
+      <Link to={ `/${recipe.type === 'comida' ? 'comidas/' : 'bebidas/'}${recipe.id}` }>
+        <p
+          data-testid={ `${index}-horizontal-name` }
+        >
+          { recipe.name }
+        </p>
+      </Link>
+      <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+      <input
+        type="image"
+        src={ shareIcon }
+        alt="compartilhar receita"
+        onClick={ () => handleShareBtn(recipe.id, recipe.type, index) }
+        data-testid={ `${index}-horizontal-share-btn` }
+      />
+      {recipe.tags && recipe.tags.map((tag) => (
+        <span
+          key={ tag }
+          data-testid={ `${index}-${tag}-horizontal-tag` }
+        >
+          {tag}
+        </span>
+      ))}
+
+    </div>
+  );
+};
+
+RecipeDoneCard.propTypes = {
+  recipe: PropTypes.objectOf(PropTypes.any).isRequired,
+  index: PropTypes.number.isRequired,
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default RecipeDoneCard;
