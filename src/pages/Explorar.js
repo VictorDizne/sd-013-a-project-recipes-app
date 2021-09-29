f1d95ff3062e566475d79221ffb35ad03a9c429e
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-09-29 16:22:04 -0300

    requisitos 67 ate 69 funcionando

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -7,6 +8,8 @@ export default function Explorar({ history }) {
   return (
     <>
       <Header pageTitle="Explorar" history={ history } />
+      <Link to="/explorar/comidas" data-testid="explore-food">Explorar Comidas</Link>
+      <Link to="/explorar/bebidas" data-testid="explore-drinks">Explorar Bebidas</Link>
       <Footer />
     </>
   );
