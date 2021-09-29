68d5ac75ffd7c6525a2e3c91cde71eddedebe744
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-09-29 19:21:21 -0300

    Iniciando requisitos 60 a 66

--- src/pages/ReceitasFavoritas.js
+++ src/pages/ReceitasFavoritas.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
 
-const ReceitasFavoritas = () => (
-  <div>
-    <HeaderWithoutSearch page="Receitas Favoritas" />
-  </div>
-);
+const ReceitasFavoritas = () => {
+  const [favorites, setFavorites] = useState('');
+
+  useEffect(() => {
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    setFavorites(favoriteRecipes);
+  });
+  // [{ id, type, area, category, alcoholicOrNot, name, image }]
+  return (
+    <div>
+      <HeaderWithoutSearch page="Receitas Favoritas" />
+      {favorites.forEach((recipe) => {
+        if (recipe.type === 'meal') {
+          return (
+            <button type="submit" key="">
+              <img src={ recipe.image } alt={ recipe.name } />
+              <h3>{ recipe.name }</h3>
+              <h3>{ recipe.category }</h3>
+              <h3>{ recipe.area }</h3>
+            </button>
+          );
+        }
+      })}
+    </div>
+  );
+};
 
 export default ReceitasFavoritas;
