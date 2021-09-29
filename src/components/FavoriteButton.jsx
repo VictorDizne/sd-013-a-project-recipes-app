53a6681aee907c344cfe50e4ee39b805044e5c1f
Icaro Ferreira <icaro.iky@gmail.com>
2021-09-29 19:48:31 -0300

    Fixed linter

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -3,36 +3,36 @@ import PropTypes from 'prop-types';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 
-function FavoriteButton({ url, recipe, isMeal }) {
+const favoriteRecipes = (clicked, recipe, isMeal) => {
+  if (!localStorage.favoriteRecipes) {
+    const arrayFavorite = [];
+    localStorage.setItem('favoriteRecipes', JSON.stringify(arrayFavorite));
+  }
+  let itemFavorite = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  if (clicked) {
+    itemFavorite = itemFavorite
+      .filter((item) => item.id !== recipe.idMeal && item.id !== recipe.idDrink);
+  }
+  const item = {
+    id: isMeal ? recipe.idMeal : recipe.idDrink,
+    type: isMeal ? 'comida' : 'bebida',
+    area: isMeal ? recipe.strArea : '',
+    category: recipe.strCategory,
+    alcoholicOrNot: isMeal ? '' : recipe.strAlcoholic,
+    name: isMeal ? recipe.strMeal : recipe.strDrink,
+    image: isMeal ? recipe.strMealThumb : recipe.strDrinkThumb,
+  };
+  if (!clicked) {
+    itemFavorite.push(item);
+  }
+  localStorage.setItem('favoriteRecipes', JSON.stringify(itemFavorite));
+};
+
+function FavoriteButton({ recipe, isMeal }) {
   const [clicked, setClicked] = useState(false);
 
-  const favoriteRecipes = () => {
-    if (!localStorage.favoriteRecipes) {
-      const arrayFavorite = [];
-      localStorage.setItem('favoriteRecipes', JSON.stringify(arrayFavorite));
-    }
-    let itemFavorite = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    if (clicked) {
-      itemFavorite = itemFavorite
-        .filter((item) => item.id !== recipe.idMeal || item.id === recipe.idDrink);
-    }
-    const item = {
-      id: isMeal ? recipe.idMeal : recipe.idDrink,
-      type: isMeal ? 'comida' : 'bebida',
-      area: isMeal ? recipe.strArea : '',
-      category: recipe.strCategory,
-      alcoholicOrNot: isMeal ? '' : recipe.strAlcoholic,
-      name: isMeal ? recipe.strMeal : recipe.strDrink,
-      image: isMeal ? recipe.strMealThumb : recipe.strDrinkThumb,
-    };
-    if (!clicked) {
-      itemFavorite.push(item);
-    }
-    localStorage.setItem('favoriteRecipes', JSON.stringify(itemFavorite));
-  };
-
   const handleClick = () => {
-    favoriteRecipes();
+    favoriteRecipes(clicked, recipe, isMeal);
     setClicked(!clicked);
   };
 
@@ -60,8 +60,8 @@ function FavoriteButton({ url, recipe, isMeal }) {
 }
 
 FavoriteButton.propTypes = {
-  url: PropTypes.string.isRequired,
   recipe: PropTypes.string.isRequired,
+  isMeal: PropTypes.string.isRequired,
 };
 
 export default FavoriteButton;
