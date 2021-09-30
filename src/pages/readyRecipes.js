59fdc2b0c2625a2b20a45d2b31cc0ceba42706da
LuizFJP <luizfjportela@gmail.com>
2021-09-30 15:21:23 -0300

    old commits

--- src/pages/readyRecipes.js
+++ src/pages/readyRecipes.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from '../components/header';
 import BttAllDrinksFood from '../components/BttAllDrinksFood';
-// import ReadyReceipesCards from '../components/ReadyReceipesCards';
+import ReadyReceipesCards from '../components/ReadyReceipesCards';
 
 function ReadyRecipes() {
+  const ingredientesDone = JSON.parse(localStorage.favoriteRecipes);
+  /* localStorage.doneRecipes =  */
+  /* useEffect(() => {}, []); */
+
   return (
     <section>
       <Header name="Receitas Feitas" search={ false } />
       <BttAllDrinksFood />
-      {/* <ReadyReceipesCards /> */}
+      <ReadyReceipesCards />
     </section>
   );
 }
