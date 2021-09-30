85aaadf9f5194a07480d9d9e03a64f6c644c077f
janderson555 <jandinsm@gmail.com>
2021-09-30 15:50:33 -0300

    finaliza headersearch

--- src/service/GetAPIDrinks.js
+++ src/service/GetAPIDrinks.js
@@ -2,28 +2,24 @@ export const fetchForIngredient = async (ingrediente) => {
   const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingrediente}`;
   const response = await fetch(endpoint);
   const { drinks } = await response.json();
-  console.log(drinks);
   return drinks;
 };
 
 export const fetchForName = async (nome) => {
   const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nome}`;
-  const { drinks } = await fetch(endpoint)
-    .then((response) => response.json());
+  const response = await fetch(endpoint);
+  const { drinks } = await response.json();
   return drinks;
 };
 
 export const fetchForFirstLetter = async (primeiraLetra) => {
   const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${primeiraLetra}`;
-  const { drinks } = await fetch(endpoint)
-    .then((response) => response.json());
-  console.log(drinks);
+  const response = await fetch(endpoint);
+  const { drinks } = await response.json();
   return drinks;
 };
 
 export const handleAPIDrinks = (param1, text) => {
-  console.log(`parametro${param1}`);
-  console.log(`texto${text}`);
   if (param1 === 'ingrediente') {
     return fetchForIngredient(text);
   }
@@ -32,8 +28,5 @@ export const handleAPIDrinks = (param1, text) => {
   }
   if (param1 === 'primeira letra' && text.length === 1) {
     return fetchForFirstLetter(text);
-  }
-  if (param1 === 'primeira letra' && text.length > 1) {
-    return global.alert('Sua busca deve conter somente 1 (um) caracter');
   }
 };
