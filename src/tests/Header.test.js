36e3837c1f3a74facf6e2a8b3c4dec94d7bc3554
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-30 09:26:32 -0300

    Coverage 62,4%

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import Header from '../components/Header';
+import '@testing-library/jest-dom';
+import FoodExploreIngredients from '../pages/FoodExploreIngredients';
 
-describe('Component Header', () => {
+describe('Componente Header', () => {
   test('Há um elemento botão de Perfil', () => {
     renderWithRouter(<Header title="Comidas" />);
 
@@ -23,5 +26,20 @@ describe('Component Header', () => {
 
     const searchTopBtn = screen.getByTestId('search-top-btn');
     expect(searchTopBtn).toBeInTheDocument();
+  });
+
+  test('Search Ingredients page has correct'
+    + 'elements and title "Explorar Ingredientes', () => {
+    const { getByText, queryByRole, history } = renderWithRouter(<FoodExploreIngredients />);
+
+    const title = getByText(/Explorar Ingredientes/i);
+
+    const searchBtn = queryByRole('img', { name: /search icon/i });
+    const profileLink = queryByRole('img', { name: /profile icon/i });
+
+    expect(title).toBeInTheDocument();
+    expect(searchBtn).not.toBeInTheDocument();
+    userEvent.click(profileLink);
+    expect(history.location.pathname).toBe('/explorar/comidas/ingredientes');
   });
 });
