30c66f309011ecaa27f76b08184af93728b55696
vdionysio <vini.dionysio@gmail.com>
2021-09-30 18:30:14 -0300

    Solving confl

--- src/services/index.js
+++ src/services/index.js
@@ -86,9 +86,3 @@ export const fetchIngredients = async (path) => {
 };
 
 export const getStorage = (key) => JSON.parse(localStorage.getItem(key));
-
-// export const fetchIArea = async (area) => {
-//   const response = await fetch(`www.themealdb.com/api/json/v1/1/filter.php?a=${area}`);
-//   const json = await response.json();
-//   return json[dataKey];
-// };
