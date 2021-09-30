3aa0dbc72ccb1cacf39b7f80b82a4c4d212bed37
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-30 11:02:10 -0300

    Coverage 75,47%

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import DoneRecipes from '../pages/DoneRecipes';
 import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
 const FILTER_BY_ALL_BTN = 'filter-by-all-btn';
 const FILTER_BY_FOOD_BTN = 'filter-by-food-btn';
@@ -137,5 +139,108 @@ describe('2 - Verifica se uma receita feita é carregada', () => {
     fireEvent.click(buttonDrink);
     const CardDrink = getAllByTestId(/-horizontal-name/i);
     expect(CardDrink.length).toBe(1);
+  });
+});
+
+const EMAIL_TEST_ID = 'email-input';
+const PASSWORD_TEST_ID = 'password-input';
+const LOGIN_BTN_TEST_ID = 'login-submit-btn';
+const MOCK_EMAIL = 'alguem@alguem.com';
+
+const PAGE_MOCK = '/receitas-feitas';
+const DONE_IMG_TESTID = '0-horizontal-image';
+
+describe('Testando mais da página de receitas feitas', () => {
+  test('O header é renderizado corretamente', () => {
+    const { getByTestId, history } = renderWithRouter(<App />);
+
+    userEvent.type(getByTestId(EMAIL_TEST_ID), MOCK_EMAIL);
+    userEvent.type(getByTestId(PASSWORD_TEST_ID), '1234567');
+    userEvent.click(getByTestId(LOGIN_BTN_TEST_ID));
+
+    history.push(PAGE_MOCK);
+
+    expect(getByTestId('profile-top-btn')).toBeInTheDocument();
+    expect(getByTestId('page-title')).toHaveTextContent('Receitas Feitas');
+  });
+
+  test('Os botões de filtro são renderizados corretamente', () => {
+    const { getByTestId, history } = renderWithRouter(<App />);
+
+    userEvent.type(getByTestId(EMAIL_TEST_ID), MOCK_EMAIL);
+    userEvent.type(getByTestId(PASSWORD_TEST_ID), '1234567');
+    userEvent.click(getByTestId(LOGIN_BTN_TEST_ID));
+
+    history.push(PAGE_MOCK);
+
+    expect(getByTestId('filter-by-all-btn')).toBeInTheDocument();
+    expect(getByTestId('filter-by-food-btn')).toBeInTheDocument();
+    expect(getByTestId('filter-by-drink-btn')).toBeInTheDocument();
+  });
+
+  test('se há uma comida feita, ela deve aparecer', async () => {
+    const { getByTestId, findAllByRole, history } = renderWithRouter(<App />);
+
+    userEvent.type(getByTestId(EMAIL_TEST_ID), MOCK_EMAIL);
+    userEvent.type(getByTestId(PASSWORD_TEST_ID), '1234567');
+    userEvent.click(getByTestId(LOGIN_BTN_TEST_ID));
+
+    history.push('/comidas/52977/in-progress');
+
+    const ingredientCheckbox = await findAllByRole('checkbox');
+    ingredientCheckbox.forEach((ing) => {
+      userEvent.click(ing);
+    });
+
+    userEvent.click(getByTestId('finish-recipe-btn'));
+
+    expect(history.location.pathname).toBe(PAGE_MOCK);
+
+    expect(getByTestId(DONE_IMG_TESTID)).toBeInTheDocument();
+  });
+
+  test('filtro por all funciona', () => {
+    const { getByTestId, history } = renderWithRouter(<App />);
+
+    userEvent.type(getByTestId(EMAIL_TEST_ID), MOCK_EMAIL);
+    userEvent.type(getByTestId(PASSWORD_TEST_ID), '1234567');
+    userEvent.click(getByTestId(LOGIN_BTN_TEST_ID));
+
+    history.push(PAGE_MOCK);
+
+    const filterByAllBtn = getByTestId('filter-by-all-btn');
+    userEvent.click(filterByAllBtn);
+
+    expect(getByTestId(DONE_IMG_TESTID)).toBeInTheDocument();
+  });
+
+  test('filtro por food funciona', () => {
+    const { getByTestId, history } = renderWithRouter(<App />);
+
+    userEvent.type(getByTestId(EMAIL_TEST_ID), MOCK_EMAIL);
+    userEvent.type(getByTestId(PASSWORD_TEST_ID), '1234567');
+    userEvent.click(getByTestId(LOGIN_BTN_TEST_ID));
+
+    history.push(PAGE_MOCK);
+
+    const filterByFoodBtn = getByTestId('filter-by-food-btn');
+    userEvent.click(filterByFoodBtn);
+
+    expect(getByTestId(DONE_IMG_TESTID)).toBeInTheDocument();
+  });
+
+  test('filtro por drink funciona', () => {
+    const { getByTestId, queryByTestId, history } = renderWithRouter(<App />);
+
+    userEvent.type(getByTestId(EMAIL_TEST_ID), MOCK_EMAIL);
+    userEvent.type(getByTestId(PASSWORD_TEST_ID), '1234567');
+    userEvent.click(getByTestId(LOGIN_BTN_TEST_ID));
+
+    history.push(PAGE_MOCK);
+
+    const filterByDrinkBtn = getByTestId('filter-by-drink-btn');
+    userEvent.click(filterByDrinkBtn);
+
+    expect(queryByTestId(DONE_IMG_TESTID)).toBeInTheDocument();
   });
 });
