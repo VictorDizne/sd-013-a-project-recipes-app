b2453105aef809de4724b9db25470fd62a97da2d
Matheus Duarte de Freitas <freickup@gmail.com>
2021-09-30 12:21:10 -0300

    Alteraçao do FoodRecipeDetails para verificacao do PR

--- src/pages/FoodRecipeDetails.jsx
+++ src/pages/FoodRecipeDetails.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+// import { useLocation } from 'react-router-dom';
 
-const FoodRecipeDetails = () => (
-  <h1>Component</h1>
-);
+const FoodRecipeDetails = () => {
+  // const location = useLocation();
+
+  return (
+    <>
+      <h1>Component</h1>
+      <p>{ window.location.origin }</p>
+      <p>{ window.location.href }</p>
+    </>
+  );
+};
 
 export default FoodRecipeDetails;
