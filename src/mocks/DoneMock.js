39269c8907ce5936a9eb34ed72b76c9343d6c1ad
Emanoel Mattos <emanoelmattos13@gmail.com>
2021-10-04 19:57:33 -0300

    adiciona parte do doneRecipesTest

---
+++ src/mocks/DoneMock.js
@@ -0,0 +1,28 @@
+const doneMock = [
+  {
+    id: '52977',
+    type: 'comida',
+    area: 'Turkish',
+    category: 'Side',
+    alcoholicOrNot: '',
+    name: 'Corba',
+    image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+    doneDate: '04/10/2021',
+    tags: [
+      'Soup',
+    ],
+  },
+  {
+    id: '12748',
+    type: 'bebida',
+    area: '',
+    category: 'Cocoa',
+    alcoholicOrNot: 'Non alcoholic',
+    name: 'Orange Scented Hot Chocolate',
+    image: 'https://www.thecocktaildb.com/images/media/drink/hdzwrh1487603131.jpg',
+    doneDate: '04/10/2021',
+    tags: [],
+  },
+];
+
+export default doneMock;
--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -8,27 +8,35 @@ import Done from '../pages/Done';
 // Helpers
 import renderWithReduxAndRouter from '../helpers/renderWithReduxAndRouter';
 
+// Mocks
+import doneMock from '../mocks/DoneMock';
+import userEvent from '@testing-library/user-event';
+
 // History
-// let mockHistory = {};
+let mockHistory = {};
 
 const PROFILE_ICON = 'profile-top-btn';
 const PAGE_TITLE = 'page-title';
 const ALL_BTN = 'filter-by-all-btn';
 const FOOD_BTN = 'filter-by-food-btn';
 const DRINK_BTN = 'filter-by-drink-btn';
+const FOOD_NAME = 'comida';
+const DRINK_NAME = 'bebida';
+const stringifyMock = JSON.stringify(doneMock);
 
-describe('Testa as funcionalidades da Favorites.jsx', () => {
+describe('Testa as funcionalidades da Done.jsx', () => {
   beforeEach(() => {
-    const { history } = renderWithReduxAndRouter(<Done />);
+    const { history } = renderWithReduxAndRouter(<Done />, {
+      items: { doneRecipes: stringifyMock },
+    });
     mockHistory = history;
   });
   it('Testa se renderiza o componente Header sem o searchButton.', () => {
     const profileImage = screen.getByTestId(PROFILE_ICON);
     const pageTitle = screen.getByTestId(PAGE_TITLE);
-
     expect(profileImage).toBeInTheDocument();
     expect(pageTitle).toBeInTheDocument();
-    expect(pageTitle).toContainHTML('Receitas Favoritas');
+    expect(pageTitle).toContainHTML('Receitas Feitas');
   });
   it('Testa se renderiza os três botões de filtragem.', () => {
     const allButton = screen.getByTestId(ALL_BTN);
@@ -39,8 +47,32 @@ describe('Testa as funcionalidades da Favorites.jsx', () => {
     expect(foodButton).toBeInTheDocument();
     expect(drinkButton).toBeInTheDocument();
   });
-  it('Testa se, ao filtrar por foods ou drinks, os resultados são filtrados', () => {
-    // const foodButton = screen.getByTestId(FOOD_BTN);
-    // const drinkButton = screen.getByTestId(DRINK_BTN);
+  it('Testa se as receitas prontas são renderizadas à tela.', () => {
+    // https://trybecourse.slack.com/archives/C0219LZPB9N/p1633048749043600
+    const comida = screen.getByTestId(FOOD_NAME);
+    const bebida = screen.getByTestId(DRINK_NAME);
+
+    expect(comida).toBeInTheDocument();
+    expect(bebida).toBeInTheDocument();
+  });
+  it('Testa se, ao clicar em Food, filtra a lista por comidas', () => {
+    const foodButton = screen.getByTestId(FOOD_BTN);
+    const comida = screen.getByTestId(FOOD_NAME);
+    const bebida = screen.getByTestId(DRINK_NAME);
+
+    userEvent.click(foodButton);
+    expect(comida).toBeInTheDocument();
+    expect(bebida).not.toBeInTheDocument();
+  });
+  it('Testa se, ao clicar em Drinks, filtra a lista por bebidas', () => {
+    // Tem um bug nesse teste, ele aprova o oposto do que precisamos.
+    const drinkButton = screen.getByTestId(DRINK_BTN);
+    const comida = screen.getByTestId(FOOD_NAME);
+    const bebida = screen.getByTestId(DRINK_NAME);
+
+    userEvent.click(drinkButton);
+
+    expect(bebida).toBeInTheDocument();
+    expect(comida).not.toBeInTheDocument();
   });
 });
