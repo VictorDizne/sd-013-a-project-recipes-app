b8f0a74bb8ddc3efd92a5331b2161f9db2319646
ALINE <hoshinoaline@yahoo.com.br>
2021-10-04 19:50:32 -0300

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-/* import MyContext from '../context/myContext'; */
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
