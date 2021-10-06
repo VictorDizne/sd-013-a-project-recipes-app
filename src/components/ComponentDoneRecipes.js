3910287715c754c14d500e6f2dacc3926c3a97ca
Cassyel <cassyel1967@gmail.com>
2021-10-05 23:27:40 -0300

    Realiza Requisito 58

--- src/components/ComponentDoneRecipes.js
+++ src/components/ComponentDoneRecipes.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDebugState } from 'use-named-state';
 import ComponentDoneCard from './ComponentDoneCard';
 
 function ComponentDoneRecipes() {
+  const [filter, setFilter] = useDebugState('filter', 'All');
+  const [array, setArray] = useDebugState('array', null);
   const storage = JSON.parse(localStorage.getItem('doneRecipes'));
-  if (storage === null) return <h1>Nenhuma Receita Encontrada</h1>;
+
+  useEffect(() => {
+    if (filter !== 'All') {
+      const storageFilter = storage.filter((recipe) => recipe.type === filter);
+      setArray(storageFilter);
+    } else {
+      setArray(storage);
+    }
+  }, [filter]);
+
+  const handleClick = ({ target }) => {
+    setFilter(target.value);
+  };
+
+  if (array === null) return <h1>Nenhuma Receita Encontrada</h1>;
   return (
     <div>
       <div>
-        <button type="button" data-testid="filter-by-all-btn">All</button>
-        <button type="button" data-testid="filter-by-food-btn">Foods</button>
-        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+        <button
+          type="button"
+          onClick={ handleClick }
+          data-testid="filter-by-all-btn"
+          value="All"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          onClick={ handleClick }
+          data-testid="filter-by-food-btn"
+          value="comida"
+        >
+          Foods
+        </button>
+        <button
+          type="button"
+          onClick={ handleClick }
+          data-testid="filter-by-drink-btn"
+          value="bebida"
+        >
+          Drinks
+        </button>
       </div>
       <div>
-        {storage.map((item, index) => (
+        {array.map((item, index) => (
           <ComponentDoneCard key={ index } currentData={ item } dataIndex={ index } />
         ))}
       </div>
