5c0e14af825d1d1dadc4a881ec3b921a921cdafa
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-10-06 17:06:38 -0400

    css Link explorar

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -2,15 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/ComponentHeader';
 import ComponentFooter from '../components/ComponentFooter';
+import './Styles/Explorar.css';
 
 function Explorar() {
   return (
     <div>
       <Header title="Explorar" hideSearch hideProfile={ false } />
-      <Link data-testid="explore-food" to="/explorar/comidas">Explorar Comidas</Link>
-      <Link data-testid="explore-drinks" to="/explorar/bebidas">Explorar Bebidas</Link>
+      <div className="container-explorar">
+        <div className="Link-container">
+          <Link
+            className="link"
+            data-testid="explore-food"
+            to="/explorar/comidas"
+          >
+            Explorar Comidas
+          </Link>
+        </div>
+        <div className="Link-container">
+          <Link
+            className="link"
+            data-testid="explore-drinks"
+            to="/explorar/bebidas"
+          >
+            Explorar Bebidas
+          </Link>
+        </div>
+      </div>
       <ComponentFooter />
-
     </div>
   );
 }
