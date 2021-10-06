778bb8a321e2b8eabcba1a3dbd2a462f03d39960
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-10-06 20:12:57 -0300

    erro import 10,11,21 devolvidos

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
