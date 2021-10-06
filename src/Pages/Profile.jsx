cb4bf49f68cc420d0a7b6b268265c69c7b0ada23
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-10-05 21:08:06 -0300

    Adiciona Requisito 82 e 83

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -7,10 +7,34 @@ export default function Profile() {
     pageName: 'Perfil',
     setIcon: false,
   };
+  const user = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
       <Header value={ pageTitle } />
-      <h2>PROFILE</h2>
+      FOODS
+      <div>
+        <h3 data-testid="profile-email">
+          { user !== null ? user.email : 'email' }
+        </h3>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Sair
+        </button>
+      </div>
       <Footer />
     </div>
   );
