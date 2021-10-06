0b88878b1c8a91b6bdf01c54a388c8d6c8b87f5f
vdionysio <vini.dionysio@gmail.com>
2021-10-06 15:26:49 -0300

    Login page tests

--- src/unit_tests/Login.test.js
+++ src/unit_tests/Login.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Login from '../pages';
+import { BrowserRouter } from 'react-router-dom';
+import { fireEvent } from '@testing-library/dom';
+import { Login } from '../pages';
 
 describe('Test on login screen, ', () => {
   it('that there are email and password inputs', () => {
@@ -12,11 +14,34 @@ describe('Test on login screen, ', () => {
     expect(emailInput).toBeInTheDocument(passwordInput);
   });
 
-  // it('that there are email and password inputs', () => {
-  //   render(<Login />);
-  // });
+  it('that there is a disabled button before validation', () => {
+    render(<Login />);
+    const loginButton = screen.getByRole('button');
+    expect(loginButton).toBeInTheDocument();
+    expect(loginButton).toBeDisabled();
 
-  // it('that there are email and password inputs', () => {
-  //   render(<Login />);
-  // });
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/senha/i);
+
+    fireEvent.change(emailInput, { target: { value: 'recipe@email.com' } });
+    fireEvent.change(passwordInput, { target: { value: '1234567' } });
+    expect(loginButton).not.toBeDisabled();
+  });
+
+  it('that the click redirects to main page', () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>,
+    );
+    const loginButton = screen.getByRole('button');
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/senha/i);
+
+    fireEvent.change(emailInput, { target: { value: 'recipe@email.com' } });
+    fireEvent.change(passwordInput, { target: { value: '1234567' } });
+    fireEvent.click(loginButton);
+
+    expect(window.location.href).toBe(`${window.location.origin}/comidas`);
+  });
 });
