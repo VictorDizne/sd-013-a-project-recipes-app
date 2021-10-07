087444ca2b00542502f291aeb8ef5aa265bf1e25
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-10-07 00:11:49 -0300

    explorar-area-ingredientes

--- src/pages/ExploreDrink.jsx
+++ src/pages/ExploreDrink.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,7 +7,20 @@ function ExploreDrink() {
   return (
     <div>
       <Header />
-      Explore Drink
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
 
