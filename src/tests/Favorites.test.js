9f5904cfc73a5dc7e6acf8e0dc2f6d60ef96ef48
junglejf <jfcchagas@id.uff.br>
2021-10-07 15:28:14 -0300

    DoneRecipes.test & Favorites.test

--- src/tests/Favorites.test.js
+++ src/tests/Favorites.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 
 // Children
 import copy from 'clipboard-copy';
